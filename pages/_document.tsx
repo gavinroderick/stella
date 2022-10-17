8eda6eb60cc31239423267365e1bbe6a1eb5f08a
Sara Emanuelsson <sara.emanuelsson@and.digital>
2022-10-17 16:27:23 +0100

    Add css variables and global styles

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=optional|Roboto:400,500,600,700&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
